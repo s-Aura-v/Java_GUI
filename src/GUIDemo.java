e57743600b0011243993bfb286ba0f618f96e7a9
s-Aura-v <slamich2@oswego.edu>
2024-02-02 20:47:22 -0500

    GridLayout

--- src/GUIDemo.java
+++ src/GUIDemo.java
@@ -4,6 +4,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -16,6 +17,7 @@ public class GUIDemo {
     private JLabel label;
     private JButton button;
     private JButton button2;
+    private JButton button3;
     private int width;
     private int height;
 
@@ -23,6 +25,7 @@ public class GUIDemo {
         frame = new JFrame();
         button = new JButton("Send!");
         button2 = new JButton("Jump Scare");
+        button3 = new JButton("Apologies");
         label = new JLabel("Hello");
         // JTextArea allows multiple lines unlike JLabel
         ta = new JTextArea("Hello. \n This is a JTextArea.");
@@ -36,17 +39,18 @@ public class GUIDemo {
     public void setUpGUI() {
         Container cp = frame.getContentPane();
 //        FlowLayout flow = new FlowLayout();
-        // More Customizable
-        BorderLayout bord = new BorderLayout();
-        cp.setLayout(bord);
+//        BorderLayout bord = new BorderLayout();
+        GridLayout grid = new GridLayout(2,3);
+        cp.setLayout(grid);
         frame.setSize(width,height);
         frame.setTitle("GUI Demo");
         // Adding content into the FlowLayout format
-        cp.add(input, BorderLayout.NORTH);
-        cp.add(label, BorderLayout.SOUTH);
-        cp.add(button, BorderLayout.WEST);
-        cp.add(button2, BorderLayout.EAST);
-        cp.add(ta, BorderLayout.CENTER);
+        cp.add(input);
+        cp.add(label);
+        cp.add(ta);
+        cp.add(button);
+        cp.add(button2);
+        cp.add(button3);
         // Remove it from the original content pane
 //        frame.add(button);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // HIDE is another option
