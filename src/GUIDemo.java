638b4f92664048eac5f8673554a4cdfeec132d8f
s-Aura-v <slamich2@oswego.edu>
2024-02-02 20:14:32 -0500

    FlowLayout

--- src/GUIDemo.java
+++ src/GUIDemo.java
@@ -1,26 +1,46 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class GUIDemo {
 
     private JFrame frame;
+    private JTextField input;
+    private JLabel label;
     private JButton button;
+    private JButton button2;
     private int width;
     private int height;
 
     public GUIDemo(int w, int h) {
         frame = new JFrame();
-        button = new JButton("Click Me");
+        button = new JButton("Click Button!");
+        button2 = new JButton("Button 2");
+        label = new JLabel("Hello");
+        // JTextField(column size);
+        // Column Size = How many letters can fix in the box
+        input = new JTextField(10);
         this.width = w;
         this.height = h;
     }
 
     public void setUpGUI() {
+        Container cp = frame.getContentPane();
+        FlowLayout flow = new FlowLayout();
+        cp.setLayout(flow);
         frame.setSize(width,height);
         frame.setTitle("GUI Demo");
-        frame.add(button);
+        // Adding content into the FlowLayout format
+        cp.add(input);
+        cp.add(label);
+        cp.add(button);
+        cp.add(button2);
+        // Remove it from the original content pane
+//        frame.add(button);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // HIDE is another option
         frame.setVisible(true);
     }
--- src/IntroTester.java
+++ src/IntroTester.java
@@ -1,6 +1,6 @@
 public class IntroTester {
     public static void main(String args[]) {
-        GUIDemo gd = new GUIDemo(400,400);
+        GUIDemo gd = new GUIDemo(500,400);
         gd.setUpGUI();
         gd.setUpButtonListener();
     }
